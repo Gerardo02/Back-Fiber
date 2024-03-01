e8962621aac645b49d66b8b47f3a5e2f3c4c22bb
Gerardo02 <gerardo.jerez@hotmail.com>
2024-02-29 18:19:42 -0700

    index

--- models/relacion_alumnos_grupos.go
+++ models/relacion_alumnos_grupos.go
@@ -5,10 +5,10 @@ import "gorm.io/gorm"
 type RelacionAlumnoGrupo struct {
 	gorm.Model
 	ID                   int              `json:"id" gorm:"primaryKey"`
-	AlumnoRefer          int              `json:"alumno_id"`
-	GruposActivosRefer   int              `json:"grupo_activo_id"`
-	GruposAprobadosRefer int              `json:"grupo_aprobado_id"`
-	EspecialidadRefer    int              `json:"especialidad_id"`
+	AlumnoRefer          int              `json:"alumno_id" gorm:"index"`
+	GruposActivosRefer   int              `json:"grupo_activo_id" gorm:"index"`
+	GruposAprobadosRefer int              `json:"grupo_aprobado_id" gorm:"index"`
+	EspecialidadRefer    int              `json:"especialidad_id" gorm:"index"`
 	Especialidad         Especialidades   `gorm:"foreignKey:EspecialidadRefer"`
 	Alumno               Alumnos          `gorm:"foreignKey:AlumnoRefer"`
 	GruposActivos        GruposActivos    `gorm:"foreignKey:GruposActivosRefer"`
